5f203f3723a864993abad38478ce78369f6ac3f8
VickaMarkina <marckina.vika99@gmail.com>
2021-08-09 14:38:04 +0300

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,19 +4,11 @@
       $('body').niceScroll();
   });
 
-  $('.header-menu__btn').click(function() {
+  $('.navbar-btn').on('click', function() {
     $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
   });
+
 
 })(jQuery);
 
-  
-$(document).ready(function(){
-
-  $('.header-navbar__btn').on('click', function() {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
-
-});
